c2b1b654ec343e0aa552834a1000ee8f0850fbc3
Daniel Miranda <danielvcmiranda@gmail.com>
2020-09-19 01:35:32 -0300

    session route

---
+++ src/routes/session_route.py
@@ -0,0 +1,21 @@
+from fastapi import APIRouter, HTTPException
+from ..controllers.session_controller import SessionController
+from ..requests.new_user_request import NewUserRequest
+from ..requests.login_request import LoginRequest
+from ..responses.user_response import UserResponse
+class SessionRoute:
+    router = APIRouter()
+
+    @staticmethod
+    @router.post(path="/new_user",response_model=UserResponse)
+    async def new_user(new_user_request: NewUserRequest):
+        response = await SessionController.create_user(new_user_request)
+        return response
+
+    @staticmethod
+    @router.post(path="/login",response_model=UserResponse)
+    async def login(login_request: LoginRequest):
+        response = await SessionController.login(login_request)
+        return response
+
+    
