7ea081af4438ce6905973fcb46083464aaaedf00
Daniel Miranda <danielvcmiranda@gmail.com>
2020-09-19 01:36:06 -0300

    aplicacao

---
+++ app.py
@@ -0,0 +1,19 @@
+from dotenv import load_dotenv
+load_dotenv()
+import uvicorn
+from fastapi import  FastAPI, Request
+from fastapi.responses import JSONResponse
+from src.routes.session_route import SessionRoute
+from src.utils.to_do_exception import ToDoException
+
+
+to_do_api = FastAPI()
+
+to_do_api.include_router(SessionRoute.router)
+
+@to_do_api.exception_handler(ToDoException)
+async def to_do_exception_handler(request: Request, exc: ToDoException):
+    return JSONResponse(content= exc.to_dict(),status_code=exc.status_code)
+
+if __name__ == "__main__":
+    uvicorn.run("app:to_do_api", host="127.0.0.1", port=5000, log_level="info",reload=True)
