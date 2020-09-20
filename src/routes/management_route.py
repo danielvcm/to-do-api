304fbaab7d90ae41d19c6bfebd47f7a718919822
Daniel Miranda <danielvcmiranda@gmail.com>
2020-09-20 02:33:09 -0300

    add tag route to management

--- src/routes/management_route.py
+++ src/routes/management_route.py
@@ -1,4 +1,5 @@
 from .session_route import SessionRoute
 from .status_route import StatusRoute
+from .tag_route import TagRoute
 class ManagementRoute:
-    routers = [SessionRoute.router,StatusRoute.router]
+    routers = [SessionRoute.router,StatusRoute.router,TagRoute.router]
