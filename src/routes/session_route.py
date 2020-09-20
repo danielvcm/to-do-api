7c2bd2ad1efa00c17db52726103b2ee298504b59
Daniel Miranda <danielvcmiranda@gmail.com>
2020-09-20 00:51:22 -0300

    fix

--- src/routes/session_route.py
+++ src/routes/session_route.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, HTTPException
+from fastapi import APIRouter
 from ..controllers.session_controller import SessionController
 from ..requests.new_user_request import NewUserRequest
 from ..requests.login_request import LoginRequest
@@ -7,13 +7,13 @@ class SessionRoute:
     router = APIRouter()
 
     @staticmethod
-    @router.post(path="/new_user",response_model=UserResponse)
+    @router.post(path="/session/new_user",response_model=UserResponse)
     async def new_user(new_user_request: NewUserRequest):
         response = await SessionController.create_user(new_user_request)
         return response
 
     @staticmethod
-    @router.post(path="/login",response_model=UserResponse)
+    @router.post(path="/session/login",response_model=UserResponse)
     async def login(login_request: LoginRequest):
         response = await SessionController.login(login_request)
         return response
