9ea6e621228651a7987c5ed576a59b057998d164
Daniel Miranda <danielvcmiranda@gmail.com>
2020-09-20 23:24:10 -0300

    adicionando checagem de usuario

--- src/services/tag_service.py
+++ src/services/tag_service.py
@@ -1,5 +1,6 @@
 from ..models.tag import Tag
 from ..utils.to_do_exception import ToDoException
+from .user_service import UserService
 class TagService:
     @staticmethod
     def get_by_id_and_id_user(id,id_user):
@@ -15,6 +16,7 @@ class TagService:
     
     @staticmethod
     def create_tag(new_tag_request):
+        UserService.check_user(new_tag_request.id_user)
         tag = Tag(id_user = new_tag_request.id_user,name = new_tag_request.name.upper())
         tag.insert_one()
         return tag
